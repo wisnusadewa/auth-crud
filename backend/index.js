5ba6c11299097ca9972b1f25dc267e6f8dd5ac79
Wisnusadewa <wisnusadewa@Wisnusadewas-MacBook-Air.local>
2024-06-08 07:46:21 +0700

    perbaikan cors dan delete cors pada route

--- backend/index.js
+++ backend/index.js
@@ -6,26 +6,6 @@ const productRoutes = require('./routes/productRoutes');
 const bodyparser = require('body-parser');
 require('dotenv').config();
 
-const app = express();
-
-// PARSING JSON
-app.use(express.json());
-
-// KONFIGURASI CORS
-app.use(
-  cors({
-    origin: 'https://auth-crud-web.vercel.app',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'token'],
-  })
-);
-
-app.use(bodyparser.json());
-
-// ROUTE
-app.use('/api/auth', authRoutes);
-app.use('/api/auth', productRoutes);
-
 // MONGODB CONNECTION
 // 'mongodb://127.0.0.1:27017/authentication'
 
@@ -34,12 +14,32 @@ mongoose
   .then(() => console.log('connect to mongodb'))
   .catch((error) => console.error('failed to connect mongodb', error));
 
+const app = express();
+
+// KONFIGURASI CORS
+app.use(
+  cors({
+    origin: ['http://localhost:5173', 'https://auth-crud-web.vercel.app'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'token'],
+    credentials: true,
+  })
+);
+
+// PARSING JSON
+app.use(express.json());
+app.use(bodyparser.json());
+
+// ROUTE
+app.use('/api/auth', authRoutes);
+app.use('/api/auth', productRoutes);
+
 // GLOBAL ERROR HANDLER
 app.use((err, req, res, next) => {
-  err.statuCode = err.statuCode || 500;
+  err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statuCode).json({
+  res.status(err.statusCode).json({
     status: err.status,
     message: err.message,
   });
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -5,7 +5,6 @@ const userController = require('../controllers/user');
 const authMiddleware = require('../utils/authMiddleware');
 
 const router = express.Router();
-router.use(cors());
 
 router.post('/signup', authControllers.signup);
 router.post('/login', authControllers.login);
