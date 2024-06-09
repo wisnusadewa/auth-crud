d7e43580c4d244f5c11d6f0b42c77d2b05d5b386
Wisnusadewa <wisnusadewa@Wisnusadewas-MacBook-Air.local>
2024-06-09 20:07:55 +0700

    origin

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ['http://localhost:5173/', 'https://auth-crud-web.vercel.app/login'],
+    origin: ['http://localhost:5173', 'https://auth-crud-web.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'token'],
     credentials: true,
