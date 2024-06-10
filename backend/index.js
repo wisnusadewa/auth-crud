afc0d3b15aa89c41c7b87dab05c40062f58ac77c
Wisnusadewa <wisnusadewa@Wisnusadewas-MacBook-Air.local>
2024-06-10 20:13:16 +0700

    penambahan logic cors

--- backend/index.js
+++ backend/index.js
@@ -10,26 +10,39 @@ require('dotenv').config();
 const app = express();
 
 // KONFIGURASI CORS MIDDLEWARE
-
-app.use(
-  cors({
-    origin: ['http://localhost:5173', 'https://auth-crud-web.vercel.app'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  })
-);
+/*******************************  CASE 1 *******************************/
+// app.use(
+//   cors({
+//     origin: ['http://localhost:5173', 'https://auth-crud-web.vercel.app'],
+//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD'],
+//     allowedHeaders: ['Content-Type', 'Authorization'],
+//   })
+// );
 
 //DISARANKAN DARI DEVTOOLS
 // Access-Control-Allow-Origin: http://localhost:5173
 // Access-Control-Allow-Methods: POST
 // Access-Control-Allow-Headers: Content-Type
 
+/*******************************  CASE 2 *******************************/
 // app.use((req, res, next) => {
 //   res.setHeader('Access-Control-Allow-Origin' : '*'),
 //   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
 //   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
 //   next();
 // });
+
+/*******************************  CASE 3 *******************************/
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*'), res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS, HEAD');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
 
 // PARSING JSON
 app.use(express.json());
